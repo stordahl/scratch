581bd7f8791aaf43403a89bacf5a7aa33f60aee9
stordahl <jacob.stordahl@stylitics.com>
2022-06-10 11:00:50 -0500

    persistance bug fix

--- src/scratch.tsx
+++ src/scratch.tsx
@@ -2,8 +2,8 @@ import { Action, ActionPanel, Form, LocalStorage } from "@raycast/api";
 import { useState, useEffect } from "react";
 
 interface Pad {
-  content:string
-  isLoading:boolean
+  content: string
+  isLoading: boolean
 }
 
 export default function Command() {
@@ -15,17 +15,17 @@ export default function Command() {
   useEffect(() => {
     (async () => {
       const stored = await LocalStorage.getItem<string>('scratch-pad');
-      if(!stored){
-        setPad((previous:Pad) => ({ ...previous, isLoading: false }));
-        return; 
+      if (!stored) {
+        setPad((previous: Pad) => ({ ...previous, isLoading: false }));
+        return;
       }
 
       try {
-        const content:string = JSON.parse(stored);
-        setPad((previous:Pad) => ({ ...previous, content, isLoading: false }));
+        const content: string = JSON.parse(stored);
+        setPad((previous: Pad) => ({ ...previous, content, isLoading: false }));
       } catch (e) {
         console.error(e)
-        setPad((previous:Pad) => ({ ...previous, content: '', isLoading: false }));
+        setPad((previous: Pad) => ({ ...previous, content: '', isLoading: false }));
       }
     })();
   }, []);
@@ -36,20 +36,21 @@ export default function Command() {
 
   return (
     <Form
-    actions={
-      <ActionPanel>
-        <Action.SubmitForm
-          title="Save Pad"
-          onSubmit={(values) => LocalStorage.setItem('scratch-pad', JSON.stringify(values.content))}
-        />
-      </ActionPanel>
-    }
+      isLoading={pad.isLoading}
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm
+            title="Save Pad"
+            onSubmit={(values) => LocalStorage.setItem('scratch-pad', JSON.stringify(values.content))}
+          />
+        </ActionPanel>
+      }
     >
-      <Form.TextArea
+      {!pad.isLoading && <Form.TextArea
         id="content"
         value={pad.content}
-        onChange={(v:string) => setPad((previous:Pad) => ({ ...previous, content: v }))}
-      />
+        onChange={(v: string) => setPad((previous: Pad) => ({ ...previous, content: v }))}
+      />}
     </Form>
   );
 }
